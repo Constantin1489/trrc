50a1d60bae321b17784d534224b852343cb24384
Constantin Hong <codesupernova@gmail.com>
2022-09-03 23:48:40 +0900

    great!

--- ankiadderall.py
+++ ankiadderall.py
@@ -6,13 +6,14 @@ import requests
 #arguments=sys.orig_argv
 arguments2=sys.argv
 #print(arguments)
-print(arguments2)
-print(arguments2[1])
-print(type(arguments2[1]))
-print(arguments2[1].split(sep='\t'))
-print(type(arguments2[1].split(sep='\t')))
+#print(arguments2)
+#print(arguments2[1])
+#print(type(arguments2[1]))
+#print(arguments2[1].split(sep='\t'))
+#print(type(arguments2[1].split(sep='\t')))
 
-print(arguments2[1].split(sep='\t'))
+#use Object or class
+card=arguments2[1].split(sep='\t')
 
 #arguments 0 ankiadderall.py file name
 #arguments 1 deckname
@@ -23,20 +24,16 @@ print(arguments2[1].split(sep='\t'))
 
 
 #BASIC notetype
-#deckname = arguments[1]
-#notetype = arguments[2]
-#front = arguments[3]
-#back = arguments[4]
-#tag = arguments[5]
+deckname = card[0]
+notetype = card[1]
+front = card[2]
+back = card[3]
+tag = card[4].split(sep=' ')
 
-#print(deckname)
-#print(notetype)
-#print(front)
-#print(back)
-#print(tag)
+print(deckname, '-', notetype, '-', front, '-', back, '-', tag)
 
-BASIC_CARD={ "action": "addNote", "version": 6, "params": { "note": { "deckName": "Default", "modelName": "Basic", "fields": { "Front": "2front content", "Back": "back content" }, "tags": [ "yomichan" ], "audio": { "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ", "filename": "yomichan_ねこ_猫.mp3", "skipHash": "7e2c2f954ef6051373ba916f000168dc", "fields": [ "Front" ] } } } }
+BASIC_CARD={ "action": "addNote", "version": 6, "params": { "note": { "deckName": deckname , "modelName": notetype, "fields": { "Front": front, "Back": back }, "tags": [ *tag ], "audio": { "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ", "filename": "yomichan_ねこ_猫.mp3", "skipHash": "7e2c2f954ef6051373ba916f000168dc", "fields": [ "Front" ] } } } }
 
 
-#r = requests.post('http://127.0.0.1:8765', json=BASIC_CARD)
-#print(r.json())
+r = requests.post('http://127.0.0.1:8765', json=BASIC_CARD)
+print(r.json())
