088d741a9f98890b0f0381d684c7dc4e702d68cc
Constantin Hong <codesupernova@gmail.com>
2022-09-03 17:21:56 +0900

    init

---
+++ ankiadderall.py
@@ -0,0 +1,5 @@
+import requests
+
+r = requests.post('http://127.0.0.1:8765', json={ "action": "addNote", "version": 6, "params": { "note": { "deckName": "Default", "modelName": "Basic", "fields": { "Front": "1front content", "Back": "back content" }, "tags": [ "yomichan" ], "audio": { "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ", "filename": "yomichan_ねこ_猫.mp3", "skipHash": "7e2c2f954ef6051373ba916f000168dc", "fields": [ "Front" ] } } } })
+
+print(r.json())
