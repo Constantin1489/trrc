b61e084fd382d36412819fecbcb90ae1ac2f0605
Constantin Hong <codesupernova@gmail.com>
2022-11-20 16:52:01 +0900

    bug : AttributeError: list object has no attribute split

--- ankiadderall.py
+++ ankiadderall.py
@@ -1,16 +1,17 @@
 #!/usr/local/bin/python
 import sys
 import requests
+import re
 
 class card:
-    def __init__(self, deck, notetype, card_list):
+    def __init__(self, deck, notetype, card_str):
         ''' no deck and notetype. because cardlist has deck and notetype. '''
         deck = deck
         notetype = notetype
         # TODO : this may cause wrong split error.
         # for example, escape key cards.
-        card_list = card_list.split(sep='\t')
-        #card  = self.check_notetype(notetype, card_list)
+        card_list = card_str.split(sep='\t')
+        #card  = self.check_notetype(notetype, card_str)
         print(self.make_card(deck, notetype, card_list))
         #print(self.make_card(deck, notetype, **card))
         #print("deck: {}\nnotetype: {}\nfront: {}\nback: {}\ntag: {}\n".format(deck, notetype, *card))
@@ -28,10 +29,15 @@ class card:
             return { 'front' : front, 'back' : back, 'tag': tag }
 
         # TODO : import config from outside.
+        # TODO : len(cloze) < 3 OR search('\t') < 2, check 'tag:' OR make error 
         if notetype in ['cloze', 'Cloze']:
-            pass
+            Text = card_list[0]
+            Extra = card_list[1]
+            tag = self.is_tag(card_list[1], card_list[-1])
+            return { 'Text' : Text, 'Extra' : Extra, 'tag': tag }
 
     def make_card(self, deck, notetype, card_list):
+        ''' return final card object to add DB. '''
         card = self.check_notetype(notetype, card_list)
         return { 'deck' : deck,\
                 'notetype' : notetype,\
@@ -45,9 +51,17 @@ class card:
         if last_item_except_tag == tag_item:
             return None
         
-
         if last_item_except_tag != tag_item:
             return tag_item
+
+
+    def contain_cloze_tag(self, cloze):
+        ''' check whether Text contains cloze tag. if not, report and skip. '''
+        # TODO [] : break if failed.
+        if re.search(r'{{c\d+::.*}}', cloze):
+            return cloze
+        else:
+            exit
 
 
 class ankiCardList:
