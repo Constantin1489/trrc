71712891feedd34493cc3b047a163b51b06d48a3
Constantin Hong <codesupernova@gmail.com>
2022-12-04 23:15:42 +0900

    BUG IndentationError
    Why:
    
    *
    File "/Users/constantinhong/TODO/ankiconnect-script/ankiadderall.py", line 64
        """
        ^
    IndentationError: expected an indented block after 'if' statement on line 63
    
    This change addresses the need by:
    
    *

---
+++ test/test-import-multiple-card-from-sys-argv.01.py
@@ -0,0 +1,24 @@
+import sys
+# insert custom module path to the end of the PATH
+sys.path.append('/Users/constantinhong/TODO/ankiconnect-script')
+import ankiadderall
+
+# test example : $python test-class-card.01.py linux cloze 'front back tag'
+
+
+# TODO [] :중복된 카드는 추가가 되지 않는다.
+# TODO [] :실패한 카드는 stderr로 표시한다.
+
+#for line in f.read().splitlines():
+
+line = sys.argv[1:]
+print(line)
+#print(line.splitlines())
+#ankiadderall.card('linux', 'Basic', line).make_card()
+a = ankiadderall.card('linux', 'Basic', line)
+# a.card doesn't print tag
+print(a.card)
+
+# ERROR :  Ctrl a "        [gnu screen] windowlist menu
+
+
