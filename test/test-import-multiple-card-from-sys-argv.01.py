80fd2f55522cbc3efe847524441e920c9f799235
Constantin Hong <codesupernova@gmail.com>
2022-12-08 17:22:43 +0900

    func : detact files or cards, and perform differently by the result.

--- test/test-import-multiple-card-from-sys-argv.01.py
+++ test/test-import-multiple-card-from-sys-argv.01.py
@@ -7,41 +7,55 @@ import os
 # test example : $python test-class-card.01.py linux cloze 'front back tag'
 
 
-# TODO [] :중복된 카드는 추가가 되지 않는다.
+# TODO [x] :중복된 카드는 추가가 되지 않는다.
 # TODO [] :실패한 카드는 stderr로 표시한다.
 
 ANKIADDERALL_CONFIG = { 'DECK': 'Linux', 'TYPE':'Basic'  }
 
-
-
-line = sys.argv[1:]
-
-if len(line) == 0:
-    print("No card {}".format(line))
-    exit(1)
-
-# if it's a file, then read file and run loop to add strings as cards.
-if os.path.isfile(*line):
-    with 
-
-
-print("{}{}".format(line, type(line)))
-
-
 # assign a card deck.
 # seek the shell env variable. if it doesn't exist, then use a compiled DECK value.
+# let's just get all environment value and check all.
 try:
     DECK = os.environ['ANKIADDERALL_DECK']
 except KeyError:
     DECK = ANKIADDERALL_CONFIG['DECK']
-    #DECK = 'Linux'
 
 # assign a card type.
 try:
     TYPE = os.environ['ANKIADDERALL_TYPE']
 except KeyError:
     TYPE = ANKIADDERALL_CONFIG['TYPE']
-    #TYPE = 'Basic'
+
+
+card_candidate = sys.argv[1:]
+
+if len(card_candidate) == 0:
+    print("No card {}".format(card_candidate))
+    exit(1)
+
+# TODO : async?
+for i in card_candidate:
+    if os.path.isfile(i):
+        print(i)
+        with open(i, encoding='unicode_escape') as f:
+            # lines is list of card in a file.
+            lines = f.read().splitlines()
+            # j is a single card.
+            for j in lines:
+                a = ankiadderall.card(DECK, TYPE, j)
+                print(a.card)
+    else:
+        print("{} type {}".format(i, type(i)))
+        #print("{} type {}".format(i.encode("unicode_escape"), type(i)))
+        print("{} ".format(i.isascii()))
+        print(isinstance(i, bytes))
+        #i = unicode(i, "utf-8")
+        a = ankiadderall.card(DECK, TYPE, i)
+        print(a.card)
+
+#print("{}{}".format(line, type(line)))
+
+
 
 
 # os.environ['ANKIADDERALL_IFS'] 를 만들어서 스페이스4가 아니면, \t으로 처리할지 정하자.
@@ -49,7 +63,8 @@ except KeyError:
 
 # 이 방식이 옳은가? 이 ankiadderall 안에 따로 datatype를 만들어서, 전부 저장했다가 한꺼번에 처리할 수 있게끔하는게 좋지 않을까?
 # python내의 for loop로 저장시켜서.. 아니면 그냥 파일을 input으로 넣어서..
-a = ankiadderall.card(DECK, TYPE, *line)
-print(a.card)
+
+#a = ankiadderall.card(DECK, TYPE, *line)
+#print(a.card)
 
 # ERROR :  Ctrl a "        [gnu screen] windowlist menu
