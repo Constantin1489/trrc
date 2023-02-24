d188572e23a64ab3b55b1f615441c7ddf8ae3eb8
Constantin Hong <codesupernova@gmail.com>
2023-02-25 05:35:47 +0900

    test/os_environ/get_os_env/refractor: 'assaign environment variable'

--- test/os_environ/get_os_env.py
+++ test/os_environ/get_os_env.py
@@ -2,11 +2,14 @@ import os
 import sys
 
 # TO get a result : export ANKIADDERAL_DECK='LINUX'
-#print(os.environ['ANKIADDERAL_DECK'])
+#print(os.environ['ANKIADDERALL_DECK'])
 
 
 # investigate type
 print("{} {} ".format(os.environ['ANKIADDERALL_DECK'], type(os.environ['ANKIADDERALL_DECK'])))
+DECK = os.environ['ANKIADDERALL_DECK'] if 'ANKIADDERALL_DECK' in os.environ.keys() else None
+TYPE  = os.environ['ANKIADDERALL_DECK'] if 'ANKIADDERALL_DECK' in os.environ.keys() else None
+print(DECK)
 
 
-#print(os.environ.keys())
+print(os.environ.keys())
