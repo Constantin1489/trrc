5d8c36458dcac3fe11a07f0185e362900d41b283
Constantin Hong <codesupernova@gmail.com>
2023-02-25 04:20:53 +0900

    test/os_environ/get_os_env.py/func: 'print current ANKIADDERALL_DECK variable of shell'

--- test/os_environ/get_os_env.py
+++ test/os_environ/get_os_env.py
@@ -2,7 +2,11 @@ import os
 import sys
 
 # TO get a result : export ANKIADDERAL_DECK='LINUX'
-print(os.environ['ANKIADDERAL_DECK'])
+#print(os.environ['ANKIADDERAL_DECK'])
+
+
+# investigate type
+print("{} {} ".format(os.environ['ANKIADDERALL_DECK'], type(os.environ['ANKIADDERALL_DECK'])))
 
 
 #print(os.environ.keys())
