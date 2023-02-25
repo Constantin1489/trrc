7efbbe030d6d221ea139568761e2ef66fa684791
Constantin Hong <codesupernova@gmail.com>
2023-02-26 02:00:08 +0900

    addstring/docs: add explanation to DECK, TYPE

--- addstring.py
+++ addstring.py
@@ -18,6 +18,7 @@ ANKIADDERALL_CONFIG = { 'DECK': 'Linux', 'TYPE':'Basic'  }
 # seek the shell env variable. if it doesn't exist, then use a compiled DECK value.
 # let's just get all environment value and check all.
 
+# if deck and type is not configed then use Anki program default Deck and card type setting.
 DECK = os.environ['ANKIADDERALL_DECK'] if 'ANKIADDERALL_DECK' in os.environ.keys() else ANKIADDERALL_CONFIG['DECK']
 TYPE  = os.environ['ANKIADDERALL_TYPE'] if 'ANKIADDERALL_TYPE' in os.environ.keys() else ANKIADDERALL_CONFIG['TYPE']
 
