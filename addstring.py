ef3d862f9a5ec3c29e956dc534e06248b79ef3e1
Constantin Hong <codesupernova@gmail.com>
2023-02-26 23:45:15 +0900

    addstring/chore: add and remove explanations

--- addstring.py
+++ addstring.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python
+
 import sys
 import re
 # insert custom module path to the end of the PATH
@@ -23,6 +24,7 @@ DECK = os.environ['ANKIADDERALL_DECK'] if 'ANKIADDERALL_DECK' in os.environ.keys
 TYPE  = os.environ['ANKIADDERALL_TYPE'] if 'ANKIADDERALL_TYPE' in os.environ.keys() else ANKIADDERALL_CONFIG['TYPE']
 
 
+# if there is a file or a string input, use it as input
 if len(sys.argv) > 1:
     card_candidate = sys.argv[1:]
 
@@ -81,15 +83,3 @@ for i in card_candidate:
         print(a.card)
 
 exit(0)
-
-
-# os.environ['ANKIADDERALL_IFS'] 를 만들어서 스페이스4가 아니면, \t으로 처리할지 정하자.
-# 아니면 그냥 \\t같은 것을 이용해서 구분시킬까?
-
-# 이 방식이 옳은가? 이 ankiadderall 안에 따로 datatype를 만들어서, 전부 저장했다가 한꺼번에 처리할 수 있게끔하는게 좋지 않을까?
-# python내의 for loop로 저장시켜서.. 아니면 그냥 파일을 input으로 넣어서..
-
-#a = ankiadderall.card(DECK, TYPE, *line)
-#print(a.card)
-
-# ERROR :  Ctrl a "        [gnu screen] windowlist menu
