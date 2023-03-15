139ae3815eebda3cf53155c7b5a1dfdea386a632
Constantin Hong <hongconstantin@gmail.com>
2023-03-15 23:23:21 +0900

    test/test-pipe_redirection/fix: Follow pytest file path convention

--- test/test-pipe_redirection.sh
+++ test/test-pipe_redirection.sh
@@ -1,3 +1,3 @@
-echo 'basic_type_front      basic_type_back    basic_type_tag' | ./addstring.py
-echo 'basic_type_front\tbasic_type_back\tbasic_type_tag' | ./addstring.py
-cat test_card_strings.py | ./addstring.py
+echo 'basic_type_front	basic_type_back	basic_type_tag' | ../addstring.py
+echo 'basic_type_front\tbasic_type_back\tbasic_type_tag' | ../addstring.py
+cat test_card_strings.py | ../addstring.py
