f0bffcc3845796dec094e562160da67d8b61db0f
Constantin Hong <hongconstantin@gmail.com>
2023-03-22 02:24:55 +0900

    test/test_classAnkiConnect/fix: Rename function and append tests

--- test/test_classAnkiConnect.py
+++ test/test_classAnkiConnect.py
@@ -13,10 +13,11 @@ userAnkico = ankiadderall.userAnkiConnect('asdfasdf', 132)
 #assert ankiadderall.userAnkiConnect('0.0.0.0', 132).returnURL() == 
 #assert ankiadderall.userAnkiConnect('0.0.0.0', 132)._webBindAddress == 
 #print('default', ankiadderall.userAnkiConnect().returnURL())
-assert ankiadderall.userAnkiConnect().returnURL() == 'localhost:8765'
+def test_get_default_get_AnkiConnect_URL():
+    assert ankiadderall.userAnkiConnect().get_AnkiConnect_URL() == 'localhost:8765'
+    assert userAnkico._webBindAddress == 'asdfasdf'
+    assert userAnkico._webBindPort == 132
+    assert userAnkico.get_AnkiConnect_URL() == 'asdfasdf:132'
+    assert ankiadderall.userAnkiConnect('0.0.0.0', 132).get_AnkiConnect_URL() == '0.0.0.0:132'
+    assert ankiadderall.userAnkiConnect('0.0.0.0', 132)._webBindAddress == '0.0.0.0'
 
-assert userAnkico._webBindAddress == 'asdfasdf'
-assert userAnkico._webBindPort == 132
-assert userAnkico.returnURL() == 'asdfasdf:132'
-assert ankiadderall.userAnkiConnect('0.0.0.0', 132).returnURL() == '0.0.0.0:132'
-assert ankiadderall.userAnkiConnect('0.0.0.0', 132)._webBindAddress == '0.0.0.0'
