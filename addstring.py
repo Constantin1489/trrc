c913a03d7a40e6e352b9033de6038973fb471827
Constantin Hong <hongconstantin@gmail.com>
2023-03-25 04:12:27 +0900

    addstring/fix: Remove a spacebar

--- addstring.py
+++ addstring.py
@@ -18,7 +18,7 @@ if __name__ == '__main__' :
     # let's just get all environment value and check all.
     # if deck and type is not configed then use Anki program default Deck and card type setting.
     DECK = os.environ['ANKIADDERALL_DECK'] if 'ANKIADDERALL_DECK' in os.environ.keys() else ANKIADDERALL_CONFIG['DECK']
-    TYPE  = os.environ['ANKIADDERALL_TYPE'] if 'ANKIADDERALL_TYPE' in os.environ.keys() else ANKIADDERALL_CONFIG['TYPE']
+    TYPE = os.environ['ANKIADDERALL_TYPE'] if 'ANKIADDERALL_TYPE' in os.environ.keys() else ANKIADDERALL_CONFIG['TYPE']
 
     card_candidate = parse_argument(sys.argv)
     parse_card(card_candidate, DECK, TYPE)
