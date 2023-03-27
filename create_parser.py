d7dbcf6e8c2c23d26a7d460d22fd1a7d7923124c
Constantin Hong <hongconstantin@gmail.com>
2023-03-28 05:37:59 +0900

    create_parser/func: Add create parser

---
+++ create_parser.py
@@ -0,0 +1,57 @@
+import argparse
+# create_parser
+def create_parser():
+    parser = argparse.ArgumentParser(
+            prog='Ankiadderall',
+            description='a command line application to create anki cards',
+            epilog='Constantin Hong'
+            )
+
+    #positional values
+    parser.add_argument(
+            'cardContents', action='store', nargs='?',
+            help=(
+            'a contents of a card'
+            ))
+    parser.add_argument(
+            '-i', '--ip',
+            action='store', dest='ip', default='127.0.0.1',
+            help=(
+            'set a AnkiConnect ip.'
+            ))
+    # mutually exclusive with positional value liked cardContents
+    parser.add_argument(
+            '-f', '--file',
+            action='store', dest='file', nargs='*',
+            help=(
+            'set a ip.'
+            ))
+
+    parser.add_argument(
+            '-c', '--config',
+            action='store', dest='config',
+            help=(
+            'set a config.'
+            ))
+
+    parser.add_argument(
+            '--alias',
+            action='store', dest='alias',
+            help=(
+            'set an alias.'
+            ))
+
+    parser.add_argument(
+            '-p', '--port',
+            action='store', dest='port', type=int, default=8765,
+            help=(
+            'set a AnkiConnect port.'
+            ))
+    parser.add_argument(
+            '-F', '--IFS',
+            action='store', dest='IFS',
+            help=(
+            'a sed-like IFS option'
+            ))
+
+    return parser
