9fb4d526b541d5be9191083a707f0e317c2122da
Constantin Hong <hongconstantin@gmail.com>
2023-03-31 19:48:39 +0900

    create_parser/feat: Add parse_argument parse_card

--- create_parser.py
+++ create_parser.py
@@ -1,5 +1,10 @@
 import argparse
-# create_parser
+import sys
+sys.path.append('/Users/constantinhong/TODO/ankiadderall')
+import os
+import re
+import ankiadderall
+
 def create_parser():
     """
      create_parser
@@ -80,6 +85,38 @@ def create_parser():
 
     return parser
 
+def parse_argument(argv):
+
+    # TODO parser
+    parser = create_parser()
+
+    # add parser
+    # if there is a file or a string input, use it as input
+    # it can't take multiple contents
+    if len(argv) > 1:
+
+        # TODO: logging
+        print("argv>1")
+        card_candidate: List[parser] = [parser.parse_args(argv[1:])]
+        return card_candidate
+
+    else:
+
+        if not sys.stdin.isatty():
+
+            # results?
+            # card.rstrip('\n').split() for parse_args?
+            card_candidate = [parser.parse_args(card.rstrip('\n')) for card in sys.stdin.readlines()]
+            # async?
+            # results?
+            return card_candidate
+
+        else:
+            print("""usage: addstring [file ...]
+            cat [file ...] | addstring""")
+            exit(2)
+
+
 # TODO: import config logic. card's deck & type => variables in bash file  OR export variables OR a temporary variable \
 # => (rc-file =>) hard coded defaults
 def get_proper_deck(argparse_deck, stdin_DECK):
@@ -118,6 +155,42 @@ def get_proper_cardType(argparse_cardType, stdin_cardType):
         return rc_cardType
 
     # return a default cardType
+    # TODO: fix default
+    # TODO: the default term depends on the language user uses may vary.
+    # ex) Korean -> '기본'
     return 'basic'
 
 
+# parse cards, card's deck and type in the input.
+def parse_card(card_candidate, DECK, TYPE):
+
+    for card in card_candidate:
+
+        # os.path.isfile(--file option)
+        if os.path.isfile(card.file):
+
+            # logging?
+            print(card.file, file=sys.stdout)
+
+            with open(card) as f:
+                lines = f.read().splitlines()
+
+            for j in lines:
+
+                # skipping an empty line.
+                if not j:
+                    continue
+
+                # if a line has cloze tag, than the line is a cloze type.
+                # note type handler
+                if re.findall(r'{{c\d::.*}}', j):
+                    TYPE = 'cloze'
+
+                a = ankiadderall.card(DECK, TYPE, j)
+                ankiadderall.create_card(ankiadderall.userAnkiConnect().get_AnkiConnect_URL(), a)
+                print(a.card, file=sys.stdout)
+
+        else:
+            a = ankiadderall.card(card.deck, card.type, card)
+            ankiadderall.create_card(ankiadderall.userAnkiConnect().get_AnkiConnect_URL(), a)
+            print(a.card, file=sys.stdout)
