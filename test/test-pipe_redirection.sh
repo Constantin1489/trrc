408ed9f3c3cc9f2ae0d385741b1ceef07fb62dd2
Constantin Hong <hongconstantin@gmail.com>
2023-04-19 02:03:15 +0900

    test-pipe_redirection.sh/test: Fix card tag to maintain consistency

--- test/test-pipe_redirection.sh
+++ test/test-pipe_redirection.sh
@@ -1,11 +1,11 @@
 echo "test 1"
-echo '1basic_type_front_normal_tab with option	basic_type_back	basic_type_tag' | ../ankiadderall/addstring.py -D "Linux::algo"
+echo '1basic_type_front_normal_tab with option	basic_type_back	test' | ../ankiadderall/addstring.py -D "Linux::algo"
 echo "test 2"
-echo '2basic_type_front_normal_tab	basic_type_back	basic_type_tag basic_type_tag2' | ../ankiadderall/addstring.py
+echo '2basic_type_front_normal_tab	basic_type_back	test' | ../ankiadderall/addstring.py
 echo "test 2-1"
-echo '3basic_type_front_normal_tab_debug	basic_type_back	basic_type_tag' | ../ankiadderall/addstring.py --debug
+echo '3basic_type_front_normal_tab_debug	basic_type_back	test' | ../ankiadderall/addstring.py --debug
 echo "test 3"
-echo '4basic_type_front_slashtab\tbasic_type_back\tbasic_type_tag' | ../ankiadderall/addstring.py
+echo '4basic_type_front_slashtab\tbasic_type_back\ttest' | ../ankiadderall/addstring.py
 echo "test 4"
 cat test_card_strings.txt | ../ankiadderall/addstring.py
 #echo "test 4-1"
@@ -19,11 +19,11 @@ echo "test 4-4"
 echo "test 4-5"
 ../ankiadderall/addstring.py -f test_card_strings2.txt
 echo "test 5"
-../ankiadderall/addstring.py '11basic_type_front_stdin	basic_type_back	basic_type_tag'
+../ankiadderall/addstring.py '11basic_type_front_stdin	basic_type_back	test'
 echo "test 6"
-../ankiadderall/addstring.py -D 'Linux::algo' '12basic_type_front_stdin_withdeck_option	basic_type_back	basic_type_tag'
+../ankiadderall/addstring.py -D 'Linux::algo' '12basic_type_front_stdin_withdeck_option	basic_type_back	test'
 echo "test 7"
-ANKIADDERALL_DECK='somedeck' ANKIADDERALL_TYPE='cloze1' ../ankiadderall/addstring.py '13env_variable_basic_type_front_stdin	basic_type_back	basic_type_tag'
+ANKIADDERALL_DECK='somedeck' ANKIADDERALL_TYPE='cloze1' ../ankiadderall/addstring.py '13env_variable_basic_type_front_stdin	basic_type_back	test'
 echo "test 8"
 ../ankiadderall/addstring.py --ip 192.123.123.123 --port 1234 --debug -f test_card_strings2.txt
 echo "test 9: cloze with basic"
@@ -34,5 +34,5 @@ cat test_cloze.txt | ../ankiadderall/addstring.py
 echo "test 11: empty lines"
 cat various_empty_lines.txt | ../ankiadderall/addstring.py
 echo "test 12: column option"
-../ankiadderall/addstring.py --column 'back:front:tag' $'back\t20testfront\tvim' --debug
-../ankiadderall/addstring.py --column 'back:front:tag' $'{{c1::back}}\t21testfront\tvim' --debug
+../ankiadderall/addstring.py --column 'back:front:tag' $'back\t20testfront\ttest' --debug
+../ankiadderall/addstring.py --column 'back:front:tag' $'{{c1::back}}\t21testfront\ttest' --debug
