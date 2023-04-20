81cd2b04b59627c6a7ba6ac68f79b3762a852605
Constantin Hong <hongconstantin@gmail.com>
2023-04-21 00:12:33 +0900

    test-pipe_redirection/fix: Change test 13 and split test 12

--- test/test-pipe_redirection.sh
+++ test/test-pipe_redirection.sh
@@ -35,6 +35,7 @@ echo "test 11: empty lines"
 cat various_empty_lines.txt | ../ankiadderall/addstring.py
 echo "test 12: column option"
 ../ankiadderall/addstring.py --column 'back:front:tag' $'back\t20testfront\ttest' --debug
+echo "test 12-1: recognized cloze but improper field."
 ../ankiadderall/addstring.py --column 'back:front:tag' $'{{c1::back}}\t21testfront\ttest' --debug
 echo "test 13: deck doesn't exist"
-../ankiadderall/addstring.py -D 'asfasdf' 'some<br>       bas     test'
+../ankiadderall/addstring.py -D 'asfasdf' 'some<br>	bas	test'
