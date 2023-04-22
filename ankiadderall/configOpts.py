32e2d36fca3ca71045a92bd4217054eb169dc19a
Constantin Hong <hongconstantin@gmail.com>
2023-04-23 02:07:09 +0900

    configOpts/feat: Init configopts

---
+++ ankiadderall/configOpts.py
@@ -0,0 +1,41 @@
+import configparser
+
+def init_config():
+    """config initialization when start the application.
+    :returns: TODO
+
+    """
+    pass
+
+def read_config(configFile):
+    """read a custome config file
+
+    Args:
+        configFile: string
+    Returns:
+
+    """
+    pass
+
+def write_config(configparser, configFile):
+    """ write config.
+    Args:
+        configparser: 
+        configFile: 
+
+    Returns:
+        None
+
+    """
+    pass
+
+def print_config(config):
+    """print config as stdout
+
+    Args:
+        config: TODO
+    Returns:
+        TODO
+
+    """
+    pass
