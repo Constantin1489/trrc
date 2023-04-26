0bdb4941c859508f5606e8e59d0c13fb959c1514
Constantin Hong <hongconstantin@gmail.com>
2023-04-27 05:30:52 +0900

    ankiadderall/fix: Change if statement check whether 'http:' or 'https' is in webbindaddress

--- ankiadderall/ankiadderall.py
+++ ankiadderall/ankiadderall.py
@@ -32,7 +32,7 @@ def userAnkiConnect(webBindAddress='localhost', webBindPort=8765):
         else, r'http://{Address}:{Port}'
         """
 
-        if ('http://' or 'https://') in webBindAddress:
+        if ('http:' or 'https') in webBindAddress[:5]:
             return r"{Address}:{Port}"
         else:
             return r"http://{Address}:{Port}"
