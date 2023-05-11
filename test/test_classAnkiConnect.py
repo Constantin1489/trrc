903ab4b84e4407dff86c2cfd54dea96de50b6895
Constantin Hong <hongconstantin@gmail.com>
2023-05-12 05:42:01 +0900

    test_classAnkiConnect/test: Add test for url

--- test/test_classAnkiConnect.py
+++ test/test_classAnkiConnect.py
@@ -1,20 +1,13 @@
 import sys
 import re
 import os
-# insert custom module path to the end of the PATH
-
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import ankiadderall.ankiadderall as ankiadderall
 import argparse
 
-userAnkico = ankiadderall.userAnkiConnect('asdfasdf', 132)
-#assert userAnkico._webBindAddress == 
-#assert userAnkico._webBindPort == 
-#assert userAnkico.returnURL() == 
-#assert ankiadderall.userAnkiConnect('0.0.0.0', 132).returnURL() == 
-#assert ankiadderall.userAnkiConnect('0.0.0.0', 132)._webBindAddress == 
-#print('default', ankiadderall.userAnkiConnect().returnURL())
 def test_get_default_get_AnkiConnect_URL():
     assert ankiadderall.userAnkiConnect() == 'http://localhost:8765'
     assert ankiadderall.userAnkiConnect('0.0.0.0', 132) == 'http://0.0.0.0:132'
     assert ankiadderall.userAnkiConnect('0.0.0.0', 132) != '0.0.0.0'
+    assert ankiadderall.userAnkiConnect('personalWeb.com', 132) == 'http://personalWeb.com:132'
+    assert ankiadderall.userAnkiConnect('https://personalWeb.com', 132) == 'https://personalWeb.com:132'
