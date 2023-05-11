435ecb104e10843bc126e61e858b555579f6ade3
Constantin Hong <hongconstantin@gmail.com>
2023-05-12 05:42:51 +0900

    test_pipe_redirection/test: Refactor data object to @pytest.mark.parametrize

--- test/test_pipe_redirection.py
+++ test/test_pipe_redirection.py
@@ -2,26 +2,20 @@
 import pytest
 import sys
 
+def make_test(test_case):
+    return {'test': test_case, 'answer': test_case + '\n'}
 
-class pipe_redirection:
-    tests = [
-        'something\tsadfas\tasdknfas',
-        '1something\tsadfas\tasdknfas',
-        '14mix_basic_type_front_cat1	basic_type_back	basic_type_tag',
-        '15cloze {{c1::asdfasdfas::}}	asdfklasj	test cloze',
-        '16mix_basic_type_front_cat2	basic_type_back	basic_type_tag',
-        'fourRow' + '\tfourRow' * 3,
-    ]
-
-def make_test(StringToTest):
-    return {'test': StringToTest, 'answer': StringToTest + '\n'}
-
-def test_pipe_redirection(capsys):
+@pytest.mark.parametrize("string_test_case",
+                         [ 'something\tsadfas\tasdknfas',
+                          '1something\tsadfas\tasdknfas',
+                          '14mix_basic_type_front_cat1	basic_type_back	basic_type_tag',
+                          '15cloze {{c1::asdfasdfas::j}	asdfklasj	test cloze',
+                          '16mix_basic_type_front_cat2	basic_type_back	basic_type_tag',
+                          'fourRow' + '\tfourRow' * 3 ])
+def test_pipe_redirection(string_test_case, capsys):
     
-    for test in pipe_redirection.tests:
+    test = make_test(string_test_case)
+    print(test['test'])
+    captured = capsys.readouterr()
 
-        test = make_test(test)
-        print(test['test'])
-        captured = capsys.readouterr()
-
-        assert captured.out == test['answer']
+    assert captured.out == test['answer']
