84a434bcb00908c0fc4a35465e0701cf50f03cdd
Constantin Hong <hongconstantin@gmail.com>
2023-05-21 04:32:06 +0900

    parserOpts/fix: Remove unliable import_if_file function's parser option

--- ankiadderall/parserOpts.py
+++ ankiadderall/parserOpts.py
@@ -164,13 +164,6 @@ def create_parser():
             ))
 
     parser.add_argument(
-            '--read-file-in-a-content',
-            action='store_true', dest='contents_file_import',
-            help=(
-            "Set to allow to replace a file in contents with a file contents. a default setting doesn't read it"
-            ))
-
-    parser.add_argument(
             '--apikey',
             action='store', dest='apikey',
             help=(
