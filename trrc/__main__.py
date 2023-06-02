4d06b53e447afa2edbcd0a830d48a376c0a9e020
Constantin Hong <hongconstantin@gmail.com>
2023-06-03 05:00:41 +0900

    __main__/docs: Add docstrings

--- trrc/__main__.py
+++ trrc/__main__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+"""
+A command line interface for trrc.
+"""
+
 import sys
 from .create_parser import parse_argument, parse_card
 
 def main():
+    """
+    A function for a command line interface.
+    """
 
     card_candidate, options = parse_argument(sys.argv[1:])
     parse_card(card_candidate, options)
